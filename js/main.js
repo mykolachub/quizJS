dcad1f1ae4e7ae8e394ac56eab6f81df9513bf24
nikolaichub <chub.mykola@ukr.net>
2021-01-07 01:23:30 +0200

    imported library from library.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+import { lib } from './library.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('welcum to my console');
+    initQuiz();
+});
+
+function initQuiz(params) {
+    console.log(lib);
+}
