560ed682a990c78a243815cd678fff38f4e4210c
nikolaichub <chub.mykola@ukr.net>
2021-01-07 18:25:34 +0200

    correct answere checker added + unsolved problem: last question does not highlight

--- js/main.js
+++ js/main.js
@@ -5,38 +5,72 @@ import { lib } from './library.js';
 document.addEventListener('DOMContentLoaded', () => {
     console.log('welcum to my console');
 
-    let plays = 0;
+    // application settings
+    const app = {
+        plays: 0, // number of games
+        library : lib, // library of questions
+        timer: 1500, // time between questions
+        form: document.getElementById('form'),
+        options: document.querySelectorAll('.quiz__input'),
+        count: document.getElementById('count'),
+        question: document.getElementById('question'),
+    }
 
-    const form = document.getElementById('form');
-    const submit = document.getElementById('submit');
-    initQuiz(plays, lib);
-    form.addEventListener('submit', ev => {
-        ev.preventDefault();
+    initQuiz(app);
+    app.form.addEventListener('submit', event => {
+        event.preventDefault();
+        const {library, options, timer} = app;
 
-        if (plays < lib.length - 1) {
-            console.log('submitted');
-            plays++;
-            initQuiz(plays, lib);
+        if (app.plays < library.length) {
+            //console.log('submitted');
+            let isChecked = false;
+            let choice;
+
+            options.forEach(option => {
+                if (option.checked) {
+                    isChecked = true;
+                    choice = option;
+                    option.checked = false;
+                }
+            });
+
+            if (isChecked) {
+                if (library[app.plays].answere == choice.value) {
+                    // correct
+                    choice.parentElement.classList.add('quiz__answere--correct');
+                } else {
+                    // wrong
+                    choice.parentElement.classList.add('quiz__answere--wrong');
+                }
+                setTimeout(() => {
+                    // updates quiz
+                    choice.parentElement.classList.remove('quiz__answere--correct', 'quiz__answere--wrong');
+                    app.plays++;
+                    initQuiz(app);
+                }, timer);
+                
+            } else {
+                alert('Choose an option first!');
+            }
+
         } else {
-            alert('End!')
+            setTimeout(() => {  
+                alert('End!');
+            }, timer);
         }
     })
 });
 
-function initQuiz(plays, lib) {
-    const count = document.getElementById('count');
-    const question = document.getElementById('question');
-    const options = document.querySelectorAll('.quiz__input');
+function initQuiz(app) {
+    const {plays, count, question, options, library} = app;
 
     count.textContent = plays + 1;
-    question.textContent = lib[plays].question;
-    options.forEach((element, index) => {
-        const inputValue = element;
-        const label = element.nextElementSibling;
-        const content = lib[plays].options[index];
+    question.textContent = library[plays].question;
+    options.forEach((option, index) => {
+        const inputValue = option;
+        const label = option.nextElementSibling;
+        const content = library[plays].options[index];
         label.textContent = content;
         inputValue.value = content;
     });
-
-    //console.log(lib[plays]);
 }
