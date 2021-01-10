fc20ce60afa66a98c819c69a7f0dc552f1cb1716
nikolaichub <chub.mykola@ukr.net>
2021-01-11 00:15:55 +0200

    score counter added + animation

--- js/main.js
+++ js/main.js
@@ -8,10 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
         constructor() {
             this.plays = 0; // KEY VARIABLE: number of games
             this.done = false; // game flag
+            this.score = 0; // number of correct answers
             this.choice = null; // user's choice
             this.checked = false; // falg false if user didnt choose an option
             this.library = null; // library of questions    
             this.timer = 1000; // time between questions
+            this.scoreTimer = this.timer / 1.25;
         }
 
         async nextGame() {
@@ -19,6 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
             return new Promise((resolve, reject) => {
                 getAnswersAndQuestions().then((res) => {
                     this.library = { ...res };
+                    console.log(res.answer);
                     resolve();
                 });
             });
@@ -40,13 +43,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         async updateQuiz() {
-            // if (this.plays == this.library.length - 1) {
-            //     this.done = true;
-            //     return;
-            // } else {
-            //     this.nextPlay();
-            //     this.initQuiz();
-            // }
             await quiz.nextGame();
             quiz.initQuiz();
         }
@@ -67,6 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const isCorrect = this.library.answer == this.choice.value;
             if (isCorrect) {
                 this.choice.parentElement.classList.add('quiz__answere--correct');
+                this.updateScore();
             } else {
                 DOM.options.forEach(option => {
                     if (this.library.answer == option.value) {
@@ -85,6 +82,14 @@ document.addEventListener('DOMContentLoaded', () => {
             }, quiz.timer);
         }
 
+        updateScore() {
+            DOM.score.parentElement.classList.add('score--updated');
+            DOM.score.textContent = `Score: ${++this.score}`;
+            setTimeout(() => {
+                DOM.score.parentElement.classList.remove('score--updated');
+            }, this.scoreTimer);
+        }
+
         // resetQuiz() {
         //     this.plays = 0;
         //     this.done = false;
@@ -100,6 +105,7 @@ document.addEventListener('DOMContentLoaded', () => {
         options: document.querySelectorAll('.quiz__input'),
         count: document.getElementById('count'),
         question: document.getElementById('question'),
+        score: document.getElementById('score'),
     }
 
     // quiz initialization
