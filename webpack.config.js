95ed9f45703ea238012d9f675dfd6db63a772f59
nikolaichub <chub.mykola@ukr.net>
2021-01-18 00:09:34 +0200

    HTMLWebpackPlugin for creating new html with fresh ./dist/main.js version in it

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
     },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            filename: '../index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
 }
